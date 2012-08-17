25f580cf149869e866e79f10af3c6482a6db6312
rlara <rlara@incaztech.com>
2012-08-17 11:41:44 -0500

    almacen empezando

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Almacen::Application.config.secret_token = '3a0cde3c09a835a70e5a585174e81dde0dd83c37fb70c274f197aa93a9446a201b3f7aa9ec699db059654e76cb5692408631f223f98a1a965a24c2d7dc712fbc'
