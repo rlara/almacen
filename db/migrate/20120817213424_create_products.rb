5543c01135e58933865dfb6b8ae598f89ffb598e
rlara <rlara@incaztech.com>
2012-08-29 20:22:56 -0500

    _form

--- db/migrate/20120817213424_create_products.rb
+++ db/migrate/20120817213424_create_products.rb
@@ -2,6 +2,7 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :name
+      t.text :description	
       t.text :sku
       t.date :date
       t.decimal :price
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120824141500) do
+ActiveRecord::Schema.define(:version => 20120817213506) do
 
   create_table "branches", :force => true do |t|
     t.string   "name"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(:version => 20120824141500) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
+    t.text     "description"
     t.text     "sku"
     t.date     "date"
     t.decimal  "price"
@@ -74,7 +75,6 @@ ActiveRecord::Schema.define(:version => 20120824141500) do
     t.boolean  "status"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
-    t.text     "description"
   end
 
 end
